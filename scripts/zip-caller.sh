7c690eaf9b695b8fa7b3555ba7407e89bb2ffe6c
Xavier <53685506+Nevermore233@users.noreply.github.com>
2025-05-13 17:30:07 +0800

    Add files via upload

---
+++ scripts/zip-caller.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -N 1                        # Request 1 node
+#SBATCH -n 28                       # Request 28 tasks
+#SBATCH --ntasks-per-node=28         # Specify 28 tasks per node
+#SBATCH --output=%j.out              # Standard output will go to jobID.out
+#SBATCH --partition=normal1         # Job partition
+#SBATCH --error=%j.err               # Standard error will go to jobID.err
+
+# ‘À–– Python Ω≈±æ
+python3 zip_caller.py -config my.config -o data/zipcall-output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
